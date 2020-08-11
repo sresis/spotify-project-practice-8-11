c33588205e922b2ff1c34d5821f5fe55fc714d2a
Stephanie Resis <sresis@umich.edu>
2020-08-11 14:12:48 -0700

    added secret files

---
+++ server.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
